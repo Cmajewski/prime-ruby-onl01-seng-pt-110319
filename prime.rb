c3425e755e57b1e80bb4f3fbcb1992b9c770ba21
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-04-28 20:56:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime? (number)
   if number==1 || number<0 || number==0
     return false
   else (2..(number-1)).all? do |n|
-  if (number%n==0) ? false: true
+   (number%n==0) ? false: true
     end
   end
 end
